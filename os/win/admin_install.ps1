3f63eb0544d34e2156e9e4d8cb8cfa1c8a03b114
Taylor McKenna <tamr43@gmail.com>
2020-04-19 17:29:29 -0500

    Cleanup Windows Bootstrap script

--- os/win/admin_install.ps1
+++ os/win/admin_install.ps1
@@ -1,16 +1,21 @@
 #!/usr/bin/env pwsh
 
-# Disable UAC & unrestrict ExecutionPolicy
+#####################################################################################################################################################
+# Disable UAC & set ExecutionPolicy to Unrestricted                                                                                                 #
+#####################################################################################################################################################
 reg.exe ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 0 /f
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force
 
-# Execute Decrapifier Powershell Script and remove after execution
+#####################################################################################################################################################
+# Windows 10 Decrapifier Script (https://community.spiceworks.com/scripts/show/4378-windows-10-decrapifier-18xx-19xx)                               #
+#####################################################################################################################################################
 (new-object net.webclient).DownloadFile('https://raw.githubusercontent.com/tamckenna/denv/master/os/win/decrapifier.ps1', 'decrapifier.ps1')
 ./decrapifier.ps1 -clearstart -settingsonly
 Remove-Item -Force decrapifier.ps1
 
-# Registry Edits
-# Re-enable Xbox Windows Services
+#####################################################################################################################################################
+# Re-enable several helpful Windows 10 Services in Registry                                                                                         #
+#####################################################################################################################################################
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\xbgm" /t REG_DWORD /f /v "Start" /d "3"
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\XblAuthManager" /t REG_DWORD /f /v "Start" /d "3"
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\XblGameSave" /t REG_DWORD /f /v "Start" /d "3"
@@ -18,20 +23,44 @@ reg add "HKLM\SYSTEM\CurrentControlSet\Services\xboxgip" /t REG_DWORD /f /v "Sta
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\XboxGipSvc" /t REG_DWORD /f /v "Start" /d "3"
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\XboxNetApiSvc" /t REG_DWORD /f /v "Start" /d "3"
 
-#Enable Developer Mode Windows 10
+#####################################################################################################################################################
+# Enable Developer Mode and Win32 long paths for NTFS                                                                                               #
+#####################################################################################################################################################
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock" /t REG_DWORD /f /v "AllowDevelopmentWithoutDevLicense" /d "1"
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\FileSystem" /t REG_DWORD /f /v "LongPathsEnabled" /d "1"
 
-# Install Chocolatey
+#####################################################################################################################################################
+# Install Global Scoop Apps                                                                                                                         #
+#####################################################################################################################################################
+scoop install 7zip git openssh ssh-copy-id --global
+
+#####################################################################################################################################################
+# Install Chocolatey Package Manager (https://chocolatey.org/)                                                                                      #
+#####################################################################################################################################################
 powershell -c "iwr -useb chocolatey.org/install.ps1 | iex"
 RefreshEnv
 
-# Install Applications through Chocolatey
-choco install 7zip git openssh -y
-choco install powershell-core -y
+#####################################################################################################################################################
+# Install Windows Features using Chocolatey                                                                                                         #
+#####################################################################################################################################################
 choco install -y Microsoft-Windows-Subsystem-Linux --source="'windowsfeatures'"
 choco install -y Microsoft-Hyper-V-All --source="'windowsfeatures'"
+
+#####################################################################################################################################################
+# Install Applications using Chocolatey                                                                                                             #
+#####################################################################################################################################################
+choco install powershell-core -y
 choco install docker-desktop -y
+
+#####################################################################################################################################################
+# Install WSL Instance (Ubuntu 18.04)                                                                                                               #
+#####################################################################################################################################################
 aria2c -o Ubuntu.appx https://aka.ms/wsl-ubuntu-1804
 Add-AppxPackage -Path Ubuntu.appx
 Remove-Item -Force Ubuntu.appx
-RefreshEnv
+echo "Will need to reboot system to utilize WSL features"
+
+#####################################################################################################################################################
+# Configurations requiring running as Administrator                                                                                                 #
+#####################################################################################################################################################
+git config --system core.longpaths true
--- os/win/setup.ps1
+++ os/win/setup.ps1
@@ -1,26 +1,50 @@
 #!/usr/bin/env pwsh
 
-# Install Scoop
+#####################################################################################################################################################
+# Install Scoop Package Manager (https://github.com/lukesampson/scoop)                                                                              #
+#####################################################################################################################################################
 iwr -useb get.scoop.sh | iex
-scoop install aria2
-scoop install curl grep sed less touch sudo
+scoop install aria2 sudo
 
+#####################################################################################################################################################
+# Execute admin install script as an Administrator                                                                                                  #
+#####################################################################################################################################################
 sudo powershell -c "iwr -useb raw.githubusercontent.com/tamckenna/denv/master/os/win/admin_install.ps1 | iex"
 RefreshEnv
 
-# Install more scoop applications
+#####################################################################################################################################################
+# Add Commonly used Scoop buckets                                                                                                                   #
+#####################################################################################################################################################
 scoop bucket add extras
 scoop bucket add java
-scoop install gow putty windows-terminal ssh-copy-id
-scoop install vscode azuredatastudio notepadplusplus sublime-text atom
-scoop install jetbrains-toolbox eclipse-jee
-scoop install dotnet-sdk nuget nuget-package-explorer
-scoop install adoptopenjdk-lts-hotspot
+scoop bucket add jetbrains
+
+#####################################################################################################################################################
+# Install Scoop Apps                                                                                                                                #
+#####################################################################################################################################################
+
+# nix Utilities
+scoop install curl wget grep sed less touch putty
+
+# Text Editors
+scoop install vscode notepadplusplus sublime-text atom
+
+# CLI Utilities
+scoop install psutils nano vim gh aws azure-cli
+
+# SDKs
+scoop install dotnet-sdk adoptopenjdk-lts-hotspot python go nvm
+
+# Dependency/Build Tools
+scoop install nuget nuget-package-explorer
 scoop install ant maven gradle
-scoop install python
-scoop install aws azure-cli
-scoop install googlechrome firefox firefox-developer brave chromium
-scoop install libreoffice-stable winmerge
-scoop install github gh postman soapui xming
-scoop install dbeaver mysql-workbench
-scoop install psutils nano vim
+
+# IDEs/SQL Editors
+scoop install jetbrains-toolbox eclipse-jee
+scoop install azuredatastudio mysql-workbench dbeaver
+
+# Development Tools
+scoop install github postman soapui xming windows-terminal libreoffice-stable winmerge
+
+# Browsers
+scoop install googlechrome firefox firefox-developer chromium brave
